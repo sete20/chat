7cf61c079e7a523ce3163dff3329e216ecd8381c
sete20 <xaxbxdxox.2015@gmail.com>
2021-03-15 05:02:34 +0200

    ..

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.post('/users', urlencodedParser, function (req, res) {
+  res.render('users', { data: req.body });
+
+  // res.send('welcome, ' + req.body.email +"  " + req.body.name);
+  console.log(req.body);
+});
+
+// POST /api/users gets JSON bodies
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/users', function(req, res, next) {
-  res.render('users', { title: 'users' });
+router.get('/', function(req, res, next) {
+  res.render('users', { title: 'Express' });
 });
 
 module.exports = router;
